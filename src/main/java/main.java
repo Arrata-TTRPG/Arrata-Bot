657e601d6af73e520ea9e527dca4779294e7679b
KalebVonBurris <kaleb.r.burris@gmail.com>
2021-02-18 17:30:37 -0900

    Adding parseInput()

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -8,8 +8,16 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
+import java.util.Locale;
 
 public class main extends ListenerAdapter {
+    private String[] keywords = {
+            "based", "i love", "now this is", "i hate you"
+            };
+    private String[] responses = {
+            "and redpilled.", "Same tbh.", "You bet your ass it is.", ":)"
+            };
+
     public static void main(String[] args) throws LoginException
     {
         if (args.length < 1) {
@@ -28,14 +36,27 @@ public class main extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event)
     {
         Message msg = event.getMessage();
-        if (msg.getContentRaw().equals("!ping"))
-        {
-            MessageChannel channel = event.getChannel();
-            long time = System.currentTimeMillis();
-            channel.sendMessage("Pong!") /* => RestAction<Message> */
-                    .queue(response /* => Message */ -> {
-                        response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
-                    });
+        if (msg.getContentRaw().contains("!A")) {
+            if(msg.getContentRaw().substring(0, 2).equals("!A")) {
+                event.getChannel().sendMessage(parseInput(msg.getContentRaw())).queue();
+            }
         }
+        else {
+            for (int i = 0; i < keywords.length; i++) {
+                if (msg.getContentRaw().toLowerCase(Locale.ROOT).contains(keywords[i]))
+                    event.getChannel().sendMessage(responses[i]).queue();
+            }
+        }
+    }
+
+    private String parseInput(String contentRaw) {
+        if (contentRaw.equals("!A"))
+            return "Wow thanks for doing literally nothing.";
+        try {
+            String contentPruned = contentRaw.substring(contentRaw.indexOf(" "));
+        } catch (Exception e) {
+            return "Error: Proper format is \"!A Arguments\"";
+        }
+        return "Wow thanks for doing literally nothing.";
     }
 }
