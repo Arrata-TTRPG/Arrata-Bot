7c74784d95b7a4b494e1e0966ae23411d866b956
KalebVonBurris <kaleb.r.burris@gmail.com>
2021-08-12 18:46:58 -0800

    Overhaul in accordance with Arrata 0.2.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,4 @@
+import com.vdurmont.emoji.EmojiParser;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
@@ -10,11 +11,18 @@ import java.util.Locale;
 
 public class main extends ListenerAdapter {
     private final String[] keywords = {
-            "based", "i love", "now this is", "i hate you", "thanks"
-            };
+            "based", "i love", "now this is", "i hate", "thanks", "hewwo", "crack", "war crime", "xd", "lol", "kill me", "floop", "amogus", "bork", "compliment me", "pog", "poggers", "(:", "{=", "genius"
+    };
     private final String[] responses = {
-            "and redpilled.", "Same tbh.", "You bet your ass it is.", ":)", ":)"
-            };
+            "and redpilled.", "Same tbh.", "You bet your ass it is.", ":)", ":)", "\"hewwo\" owo die", "*I love crack*.\n:)", "Very funny, funny man.",
+            "XD RAWR LOL", "Commit die.", "I wish I could.\n:)", "Wow how original and quirky.",
+            "GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD GET OUT OF MY HEAD",
+            "Dogs are not allowed. Unless you're ordering.", "You're a malformed brainlet.", "POG?!", "POGGERS?!", "It's :) retard.\n:)", "It's :) retard.\n:)", "Ur mom is a genius."
+    };
+    private final String[] banned = {
+            "rawr", "i hate you kaleb", "fuck kaleb", "kill yourself", "nigger", "faggot", "fuck", "damn", "shit", "cunt", "ass", "dick", "cock", "uwu", "owo", "jew", "꜀ᵣᵢₙ₉ₑ", "ᶜʳᶦⁿᵍᵉ",
+            "I love based crack. Thanks for the war crimes xd lol rawr kill me tho. Haha amogus floop, bork bork, please compliment me for my pog poggers (: {=", "ⱼₑᵥᵥ", "ₒwₒ", "ᵤwᵤ", "cringe"
+    };
     private final NovoTools tools = new NovoTools();
 
     public static void main(String[] args) throws LoginException
@@ -32,20 +40,43 @@ public class main extends ListenerAdapter {
                 .build();
     }
 
-    public void onMessageReceived(MessageReceivedEvent event)
-    {
+    public void onMessageReceived(MessageReceivedEvent event) {
         Message msg = event.getMessage();
+        if (msg.getAuthor().isBot())
+            return;
+        System.out.println(msg.getContentRaw() + " " + event.getAuthor().getId());
         if (msg.getContentRaw().contains("!A")) {
             if(msg.getContentRaw().startsWith("!A")) {
                 event.getChannel().sendMessage(parseInput(msg.getContentRaw().toUpperCase(Locale.ROOT))).queue();
             }
         }
         else {
+            try {
+                if (hasEmoji(msg.getContentRaw().toUpperCase(Locale.ROOT))) {
+                    event.getChannel().sendMessage("Did you just use an emoji?\nWhat the fuck is wrong with you?\n:(").queue();
+                    event.getMessage().delete().queue();
+                    return;
+                }
+            } catch (Exception ignored) {}
+            for (String s : banned) {
+                if (msg.getContentRaw().toLowerCase(Locale.ROOT).contains(s) &&
+                        !event.getAuthor().getId().equals("266478982294274048")) {
+                    event.getChannel().sendMessage("You just said an uh oh bad stinky word. I deleted it so that no one else has to suffer your cringe.\n:)").queue();
+                    event.getMessage().delete().queue();
+                    return;
+                }
+            }
             for (int i = 0; i < keywords.length; i++) {
-                if (msg.getContentRaw().toLowerCase(Locale.ROOT).contains(keywords[i]))
+                if (msg.getContentRaw().toLowerCase(Locale.ROOT).contains(keywords[i])) {
                     event.getChannel().sendMessage(responses[i]).queue();
+                    return;
+                }
             }
         }
+    }
+
+    private boolean hasEmoji(String message) {
+        return EmojiParser.extractEmojis(message).size() > 0;
     }
 
     private String parseInput(String contentRaw) {
@@ -53,48 +84,49 @@ public class main extends ListenerAdapter {
             return "Wow thanks for doing literally nothing.";
         String[] stats;
         try {
+            // Gets rid of the "!A".
             String contentPruned = contentRaw.substring(contentRaw.indexOf(" ") + 1);
+            // Splits the input.
             stats = contentPruned.split(" ");
         } catch (Exception e) {
-            return "Error: Proper format is \"!A argument1 argument2\". Remember that stats/difficulties cannot be greater than A and 6.";
+            // If the format is wrong, we make fun of them.
+            return "Error: Proper format is \"!A argument1 argument2\".";
         }
 
-        String difficulty = "";
+        if (stats.length > 2)
+            return "Error: More than two arguments provided.";
 
-        for (String stat : stats) {
-            if (!tools.checkStatValid(stat)){
-                if (!tools.checkDifficultyValid(stat)) {
-                    return "Error: " + stat + " is not a proper stat/difficulty.";
-                }
-                difficulty = stat;
+        if (stats[0].equals("CHAR")) {
+            if (stats.length > 1) {
+                return tools.createCharacter(Integer.parseInt(stats[1]));
             }
+            else return tools.createCharacter(1);
         }
 
-        if (stats.length == 1 && tools.checkStatValid(stats[0]))
-            return "I can't really do anything with a single stat.";
-        if (stats.length == 1 && tools.checkDifficultyValid(stats[0]))
-            return "I can't really do anything with a single difficulty.";
+        if (stats.length == 1 && tools.checkStatValid(stats[0])) {
+            String[] successes = tools.roll(stats[0]);
+            return "Rolling **" + stats[0] + "**.\n" + "Results: " + successes[0] + "\n**" + successes[1] + "** success(es)";
+        }
 
-        int[] results;
-        if (stats.length == 2 && !difficulty.equals("")) {
-            results = tools.startDifficultyRoll(stats);
-            String output;
-            if (results[0] < results[1])
-                output = "you failed by " + (results[1] - results[0]) + " successes.";
+        if (stats.length == 2 && tools.checkStatValid(stats[0]) && tools.checkObValid(stats[1])) {
+            String[] successes = tools.roll(stats[0]);
+            String result = "Rolling **" + stats[0] + "** vs **Ob " + stats[1] + "**\n" + "Results: " + successes[0] + "\n**" + successes[1] + "** success(es)\n";
+            if (Integer.parseInt(successes[1]) < Integer.parseInt(stats[1])) {
+                return result + "**Failure**";
+            }
             else
-                output = "you succeeded by " + (results[0] - results[1]) + " successes.";
-            return "You got " + results[0] + " successes against a " + difficulty + "; " + output;
+                return result + "**Success!**";
         }
-        else {
-            StringBuilder output = new StringBuilder();
-            try {
-                results = tools.statContest(stats);
-                for (int i = 1; i < results.length + 1; i++)
-                    output.append("Contestant ").append(i).append(" got ").append(results[i - 1]).append(" successes.\n");
-                return output.toString();
-            } catch (Exception e) {
-                return ("Error: Stats provided messed up. Do better next time.");
+
+        if (stats.length == 2 && tools.checkStatValid(stats[1]) && tools.checkObValid(stats[0])) {
+            String[] successes = tools.roll(stats[1]);
+            String result = "Rolling **" + stats[1] + "** vs **Ob " + stats[0] + "**\n" + "Results: " + successes[0] + "\n**" + successes[1] + "** success(es)\n";
+            if (Integer.parseInt(successes[1]) < Integer.parseInt(stats[0])) {
+                return result + "**Failure**";
             }
+            else
+                return result + "**Success!**";
         }
+        return "Error: Parsing Error.";
     }
 }
